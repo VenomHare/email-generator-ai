58a616a28d84923c121a5a52b8b89df89a25dd34
Sarthak <sarthakkadam147@gmail.com>
2025-08-15 06:30:32 +0000

    removed dist

--- server/dist/chat.d.ts
+++
@@ -1,6 +0,0 @@
-interface ExtractedData {
-    extractedContent: string[];
-    modifiedString: string;
-}
-export declare function extractTagData(inputString: string, tagName: string): ExtractedData;
-export {};
